4125f9e5bb29b7d5612790b174e14bbbcea59322
keyamoni05866 <keya05866@gmail.com>
2023-03-31 18:01:51 +0600

    navbar added

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div>
+            <nav class="navbar navbar-light  bg-light justify-content-between">
+        <a className="navbar-brand ms-4 fs-3 fw-semibold fst-italic">Programming BookStore</a>
+        <div className="nav-image">
+            <img src="https://images.unsplash.com/photo-1632333528660-f0bed1510411?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=8" alt="" />
+        </div>
+      </nav>
+        </div>
+    );
+};
+
+export default Header;
